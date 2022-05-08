d7d1f4ca005bb6f21d70314e7b920bfc4f018cc5
Hossain302044 <Hossain30204>
2022-05-08 17:00:21 +0600

    home page 6 product show

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 
@@ -20,11 +20,36 @@ async function run() {
         await client.connect();
         const productsCollection = client.db('ProductManagement').collection('products');
 
+        app.get('/allproducts', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const allProduts = await cursor.toArray();
+            res.send(allProduts);
+        })
         app.get('/products', async (req, res) => {
             const query = {};
-            const cusror = productsCollection.find(query);
-            const produts = await cusror.toArray();
+            const cursor = productsCollection.find(query);
+            const produts = await cursor.limit(6).toArray();
             res.send(produts);
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+        app.get('/allproducts', async (req, res) => {
+            const search = req.query.email;
+            console.log(search);
+            const matched = await productsCollection.filter(product => product.email.includes(search));
+            res.send(matched);
+        })
+
+        //post
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
         })
     }
     finally {
@@ -37,8 +62,8 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('warehouse management server');
-})
+});
 
 app.listen(port, () => {
     console.log('Server Running Port', port);
-})
+});
